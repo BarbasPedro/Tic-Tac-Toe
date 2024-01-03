3fd854452410e5d10504638feb37e8a8ef06bd7c
Pedro Barbas <pedrodrlvbarbas@gmail.com>
2024-01-03 12:40:05 -0300

    Tic Tac Toe

---
+++ src/App.js
@@ -0,0 +1,15 @@
+export default function Square() {
+  return (
+    <>
+      <button className="square">1</button>
+      <button className="square">2</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+    </>
+  );
+}
